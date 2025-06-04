3ca4e0946b88b202a718250e58846cf88e0542e1
Andrey <loseyourself1290@gmail.com>
2025-06-04 10:47:03 +0300

    Update config.py

--- forest_test/config.py
+++ forest_test/config.py
@@ -3,9 +3,7 @@
 #-----------------------------------------------------------------------------------------
 DETECTORS_PATH = 'moderate/test_server/detectors/yolo/src/' # Указать полный путь!!!!!
 
-AWS_ACCESS_KEY_ID = 'I92ARONASP2WFX8523FJ'
-AWS_SECRET_ACCESS_KEY = 'PSqfRVxFD6cPENiPw9aCMbDOub2FkZg9j0L6evcp'
-S3_ENDPOINT_URL = 'https://imbalanced.tech/'
+
 
 BUCKET_NAME = 'public'
 
